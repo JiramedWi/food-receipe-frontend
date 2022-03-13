7d40425c0dc2159ce9e86b02167fc20909d01570
Jiramed <jiramed_wi@cmu.ac.th>
2022-03-13 20:02:04 +0700

    implement login, regis

--- src/container/Login/ViewMode.js
+++ src/container/Login/ViewMode.js
@@ -1,24 +1,37 @@
 import { useState } from "react";
 import { useContextAuthManager } from "../../authentication";
 import { successToast } from "../../componenet/Toast/SuccessToast";
+import { loginApi } from "../../service/login";
 
 const ViewModel = () => {
   const [loginProps, setLoginProps] = useState({ username: "", password: "" });
-  const { setIsLoggedIn } = useContextAuthManager();
+  const { setIsLoggedIn, setUser } = useContextAuthManager();
 
   const onSubmit = (e) => {
     e.preventDefault();
 
     if (loginProps.username && loginProps.password) {
-      setIsLoggedIn(true);
-      successToast({
-        content: (
-          <>
-            <p className="mb-[2px] font-semibold text-gray-900">Log in</p>
-            <p className="mt-[2px] text-gray-700">You are logged in.</p>
-          </>
-        ),
-      });
+      const payload = {
+        username: loginProps.username,
+        password: loginProps.password,
+      };
+
+      loginApi(payload)
+        .then(response => {
+          localStorage.setItem("username", response.data.username)
+          setUser({ username: response.data.username })
+          setIsLoggedIn(true)
+        })
+        .then(() => successToast({
+          content: (
+            <>
+              <p className="mb-[2px] font-semibold text-gray-900">Log in</p>
+              <p className="mt-[2px] text-gray-700">You are logged in.</p>
+            </>
+          ),
+        }))
+        .catch(err => console.log('err', err))
+
     } else {
       alert("please input username & password to login");
     }
--- src/service/register.js
+++ src/service/register.js
@@ -2,9 +2,5 @@ import api from "./api";
 
 export const registerApi = async (params) => {
   return await api
-    .post("/", {
-      params: params,
-    })
-    .then((response) => response.data)
-    .catch((error) => console.log("error", error));
+    .post("/api/user", null, { params: params })
 };
